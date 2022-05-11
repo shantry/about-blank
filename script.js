a5b11dc2581972d3e79fe53e9bdfb40fa5defb6e
ThatGuyAid3n <83622490+Aid3n-cmd@users.noreply.github.com>
2022-05-11 12:33:41 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,95 @@
+function myFunction(const nogg = window.open("about:blank", '_blank', "popup");
+
+           setTimeout(() => {   nogg.document.write(`
+								<!DOCTYPE html>
+								<style>
+                body {
+                  margin: 0;
+                }
+                iframe {
+                  display: block;
+                  border: none;
+                  height: 100vh;
+                  width: 100vw;
+                }
+.custom_menu {
+    width: 100%;
+    margin: 0 auto;
+    text-align: center;
+}
+.custom_menu ul {
+    margin: 0px;
+    padding: 0px;
+    display: inline-block;
+}
+.custom_menu li {
+    float: left;
+    font-size: 16px;
+    color: #f1f1f1;
+    padding-left: 100px;
+    padding-right: 100px;
+}
+ ul, li, ol {
+    margin: 0px;
+    padding: 0px;
+    list-style: none;
+    color: white;
+} 
+ul {
+    display: block;
+    list-style-type: disc;
+    margin-block-start: 1em;
+    margin-block-end: 1em;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+    padding-inline-start: 40px;
+    
+}
+li {
+   color: white;
+  font-family: "Helvetica";
+  /* background-color: #210535; */
+  padding: 10px;
+  border-radius: 10px;
+  text-decoration: none;
+  transition: 0.5s;
+}
+a {
+  color: white;
+  font-family: "Helvetica";
+  /* background-color: #210535; */
+  padding: 10px;
+  border-radius: 10px;
+  text-decoration: none;
+  transition: 0.5s;
+}
+a:hover {
+  color: grey;
+  transition: 0.5s;
+  cursor: pointer;
+}
+.header_section_top {
+  position: fixed;
+    animation-name: fadein;
+    animation-duration: 2s;
+    width: 100%;
+    float: left;
+    background-color: rgb(90, 24, 154);
+    clip-path: polygon(0 0, 100% 0, 96% 100%, 3% 100%);
+    height: auto;
+   padding: -25px 0px;
+    color: white;
+}
+                </style>
+                <head>
+<title>R</title> 
+
+                      </head> 
+                        <body> 
+ 
+								<iframe sandbox=" allow-downloads allow-forms allow-modals allow-orientation-lock allow-pointer-lock allow-presentation allow-scripts allow-same-origin allow-popups" src=minc.misleading.tk>
+</iframe>
+</body`)
+                            }) {
+
+                      }
